28501b5034f9e664761bc7cfc92bc2ab29db7b4e
unknown <tgdd.hieu2005@gmail.com>
2023-12-01 11:57:47 +0700

    baitap7

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Baitap1 from '../src/pages/baitap1/Baitap1/'
+// import { Login } from '@mui/icons-material'
+import Login from '../src/pages/baitap1/Home/Login/'
+import SignUp from './pages/baitap1/Home/Singup'
+import Baitap2 from './pages/baitap2/Baitap2'
+import Heatder from './pages/baitap1/plojeck/heatder'
+
+export default function App() {
+  return (
+    <>
+      {/* <Baitap1 /> */}
+      {/* <Login /> */}
+      {/* <SignUp /> */}
+      {/* <Baitap2 /> */}
+      <Heatder />
+    </>
+  )
+}
---
+++ src/pages/baitap2/Baitap2.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+export default function Baitap2() {
+  return (
+    <>
+      <div>
+        <h3>My Shop</h3>
+        <h4>Cart z</h4>
+        <h2>Product List</h2>
+        <div>
+          <img
+            src="https://cdnuploads.aa.com.tr/uploads/Contents/2020/06/01/thumbs_b_c_dc24a18cc233bd960f410911f5d39bf2.jpg"
+            alt=""
+          />
+          <div>
+            <p>Product 1</p>
+            <p>$10.00</p>
+          </div>
+          <button>Add to Cart</button>
+        </div>
+        <div>
+          <img
+            src="https://icdn.ensonhaber.com/resimler/galeri/1_11195.jpg"
+            alt=""
+          />
+          <div>
+            <p>Product 1</p>
+            <p>$10.00</p>
+          </div>
+          <button>Add to Cart</button>
+        </div>
+        <div>
+          <img
+            src="https://img.paratic.com/dosya/2015/03/dunyanin-en-pahali-limuzinleri-1024x683.jpg"
+            alt=""
+          />
+          <div>
+            <p>Product 1</p>
+            <p>$10.00</p>
+          </div>
+          <button>Add to Cart</button>
+        </div>
+      </div>
+    </>
+  );
+}
